1824a2dcbe98782b1ca77cf271038c84943e8bf3
y-a-h-o-o <wuwuwahoo@gmail.com>
2025-09-30 22:09:41 -0400

    Frame Rate handling done. Will look into VSynce too.

--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,10 @@ struct SDL_context {
 	SDL_Renderer *renderer; 
 }; 
 
+// Global variables for screen width and height.
 constexpr int screen_width = 640; 
 constexpr int screen_height = 480; 
+constexpr int fps = 60;
 
 bool init_context(SDL_context& ctx) {
 	if(!SDL_Init(SDL_INIT_VIDEO)) {
@@ -41,13 +43,35 @@ int main(int arc, char** arv) {
 	}
 	
 	bool quit = false; 
-	
+
 	// SDL_Event object to check for events; I am using this instead of SDL_Callbacks
 	// Because it is easier for me to understand.
 	SDL_Event e; 
 	SDL_zero(e); 
-	
+
 	SDL_FRect test_rect; 
+	test_rect.x = 0; 
+	test_rect.y = (screen_height / 2) - 50; 
+	test_rect.w = test_rect.h = 100; 
+
+	Uint64 prev_time = SDL_GetTicksNS();
+	Uint64 delta = 0; 
+	Uint64 timer = 0; 
+	int draw_count = 0; 
+
+	auto render = [&]() {
+		SDL_SetRenderDrawColor(app.renderer, 0, 0, 0, SDL_ALPHA_OPAQUE); 
+		SDL_RenderClear(app.renderer); 	
+		
+		test_rect.x += 5; 
+		if(test_rect.x >= screen_width) {
+			test_rect.x = -test_rect.w;
+		}
+
+		SDL_SetRenderDrawColor(app.renderer, 0, 0, 255, SDL_ALPHA_OPAQUE); 
+		SDL_RenderFillRect(app.renderer, &test_rect); 
+		SDL_RenderPresent(app.renderer);
+	}; 
 
 	while(!quit) {
 		while(SDL_PollEvent(&e)) {
@@ -56,17 +80,24 @@ int main(int arc, char** arv) {
 			}	
 		}
 		
-		const Uint64 now = SDL_GetTicks(); 
-		SDL_SetRenderDrawColor(app.renderer, 0, 0, 0, SDL_ALPHA_OPAQUE); 
-		SDL_RenderClear(app.renderer); 	
+		constexpr Uint64 ns_per_frame = (1e9) / fps; 	
+		const Uint64 now = SDL_GetTicksNS(); 
+		delta += now - prev_time; 
+		timer += now - prev_time; 
+		prev_time = now;
 
-		test_rect.x = (now % screen_width); 
-		test_rect.y = (screen_height / 2) - 50; 
-		test_rect.w = test_rect.h = 100; 
+		if(delta >= ns_per_frame) {
+			render();
+			draw_count++;
+			delta -= ns_per_frame; 
+		}
+	
+		if(timer >= 1e9) {
+			SDL_Log("FPS: %d", draw_count); 
+			timer = 0; 
+			draw_count = 0; 
+		}
 		
-		SDL_SetRenderDrawColor(app.renderer, 0, 0, 255, SDL_ALPHA_OPAQUE); 
-		SDL_RenderFillRect(app.renderer, &test_rect); 
-		SDL_RenderPresent(app.renderer); 
 	}	
 	
 	delete_context(app); 	
